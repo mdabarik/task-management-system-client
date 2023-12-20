e1839f6225fd73df8db47a0ae53cf64a065954e3
mdabarik <mdabarik19@gmail.com>
2023-12-21 00:16:03 +0600

    basic components and router added

---
+++ src/routes/Router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default router;
